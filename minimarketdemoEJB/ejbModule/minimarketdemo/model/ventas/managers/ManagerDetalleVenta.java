b084a1417d31ba374af828f6b51bfce5dcb03567
StevenH1997 <60419608+StevenH1997@users.noreply.github.com>
2021-03-22 01:31:59 -0500

    Implementacion del managerDetalleVenta

---
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/ventas/managers/ManagerDetalleVenta.java
@@ -0,0 +1,20 @@
+package minimarketdemo.model.ventas.managers;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerDetalleVenta
+ */
+@Stateless
+@LocalBean
+public class ManagerDetalleVenta {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerDetalleVenta() {
+        // TODO Auto-generated constructor stub
+    }
+
+}

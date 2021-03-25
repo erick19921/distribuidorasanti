c290a41fb27f492927441cb307dfe89207028583
AnthonyGrandaH <apgrandah@utn.edu.ec>
2021-03-24 19:54:44 -0500

    ManagerVenVenta Cabecera de la venta

--- minimarketdemoEJB/ejbModule/minimarketdemo/model/ventas/managers/ManagerVenVenta.java
+++ minimarketdemoEJB/ejbModule/minimarketdemo/model/ventas/managers/ManagerVenVenta.java
@@ -1,7 +1,14 @@
 package minimarketdemo.model.ventas.managers;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import minimarketdemo.model.core.entities.VenCliente;
+import minimarketdemo.model.core.entities.VenVenta;
+import minimarketdemo.model.core.managers.ManagerDAO;
 
 /**
  * Session Bean implementation class ManagerVenVenta
@@ -13,8 +20,30 @@ public class ManagerVenVenta {
     /**
      * Default constructor. 
      */
+	@EJB
+    ManagerDAO mDAO;
     public ManagerVenVenta() {
         // TODO Auto-generated constructor stub
     }
 
+    
+    public List<VenVenta> findAllVenta(){
+    	return mDAO.findAll(VenVenta.class, "idVenta");
+    }
+    
+    public void insertarCabeceraVenta(VenVenta nuevaVenta, String cedula) throws Exception {
+    	VenCliente cliente = (VenCliente)mDAO.findById(VenCliente.class, cedula);
+    	nuevaVenta.setVenCliente(cliente);
+    	mDAO.insertar(nuevaVenta);
+    }
+    
+    public void eliminarCabeceraVenta(int idVenta) throws Exception{
+    	VenVenta cabeceraVenta = (VenVenta)mDAO.findById(VenVenta.class, idVenta);
+    	mDAO.eliminar(VenVenta.class, cabeceraVenta.getIdVenta());
+    }
+    
+    public VenVenta findCabeceraVentaById(int idVenta) throws Exception{
+    	VenVenta busqVenta = (VenVenta)mDAO.findById(VenVenta.class, idVenta);
+    	return busqVenta;
+    }
 }
